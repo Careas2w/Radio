e16ec3603d162a4bd6fef18b0f64821b3835d005
Careas2w <careaspartulepe@gmail.com>
2026-04-22 11:56:00 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,44 @@
+const CACHE = "radios-libres-v3";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./sw.js",
+  "./favicon.ico",
+  "./assets/icons/icon.svg",
+  "./assets/icons/icon-152.png",
+  "./assets/icons/icon-167.png",
+  "./assets/icons/icon-180.png",
+  "./assets/icons/icon-192.png",
+  "./assets/icons/icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  const req = event.request;
+  if (req.method !== "GET") return;
+
+  event.respondWith(
+    caches.match(req).then((cached) => {
+      return cached || fetch(req).catch(() => {
+        if (req.mode === "navigate") return caches.match("./index.html");
+        return caches.match(req);
+      });
+    })
+  );
+});
